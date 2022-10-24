6b077c1d38499b8732ebd922f5c1ba47678ca1dd
Pau <103753396+pau-develop@users.noreply.github.com>
2022-10-24 20:59:31 +0200

    Created connection to DB and fetch scores function

--- components/GameTopScores/GameTopScores.tsx
+++ components/GameTopScores/GameTopScores.tsx
@@ -1,7 +1,42 @@
+import connectDB from "../../database/connectDB";
+import { useState, useCallback, useEffect } from "react";
+
+interface scoresProps {
+  name: string;
+  player: number;
+  score: number;
+}
+
 const GameTopScores = () => {
+  const [scores, setScores] = useState<scoresProps[] | undefined>();
+  const fetchScores = useCallback(async () => {
+    const response = await fetch("/api/scores");
+    const { myScores } = await response.json();
+    console.log(myScores);
+    setScores(myScores);
+  }, []);
+
+  useEffect(() => {
+    fetchScores();
+  }, [fetchScores]);
+
   return (
-    <div className="menu-wrap__about">
+    <div className="menu-wrap__scores">
       <h2>Top Scores</h2>
+      {scores !== undefined && (
+        <ul>
+          {scores.map((element, index) => {
+            return (
+              <li key={index}>
+                <span>{index + 1}</span>
+                <span>{element.player}</span>
+                <span>{element.name}</span>
+                <span>{element.score}</span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
---
+++ pages/api/scores.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDB from "../../database/connectDB";
+
+const scores = async (request: NextApiRequest, response: NextApiResponse) => {
+  const client = await connectDB();
+  const db = client.db("kungfu-skate");
+  switch (request.method) {
+    case "GET":
+      const scores = await db.collection("scores").find({}).toArray();
+      const myScores = new Array(scores.length);
+      for (let i = 0; i < scores.length; i++) {
+        myScores[i] = {
+          name: scores[i].name,
+          player: scores[i].player,
+          score: scores[i].score,
+        };
+      }
+      response.json({ myScores });
+      break;
+  }
+};
+
+export default scores;
