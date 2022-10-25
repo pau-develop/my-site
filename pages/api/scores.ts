067a2edd5a28fec4047e010d5c450d7fb87e82e1
Pau <103753396+pau-develop@users.noreply.github.com>
2022-10-25 13:21:37 +0200

    Adding sort method to DB query

--- pages/api/scores.ts
+++ pages/api/scores.ts
@@ -6,7 +6,11 @@ const scores = async (request: NextApiRequest, response: NextApiResponse) => {
   const db = client.db("kungfu-skate");
   switch (request.method) {
     case "GET":
-      const scores = await db.collection("scores").find({}).toArray();
+      const scores = await db
+        .collection("scores")
+        .find({})
+        .sort({ score: -1 })
+        .toArray();
       const myScores = new Array(scores.length);
       for (let i = 0; i < scores.length; i++) {
         myScores[i] = {
