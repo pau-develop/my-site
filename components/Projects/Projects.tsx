d174affba9c54bdd3279ddab8f3c98f037128516
Pau <103753396+pau-develop@users.noreply.github.com>
2022-10-28 13:51:11 +0200

    Passing state and action from parent to child

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -4,10 +4,18 @@ import { useState } from "react";
 
 const Projects = () => {
   const [currentProject, setCurrentProject] = useState(0);
+
+  const handleMenuClick = (index: number) => {
+    setCurrentProject(index);
+  };
+
   return (
     <ProjectsStyled className="projects">
       <div className="projects__wrap">
-        <ProjectsList currentProject={currentProject} />
+        <ProjectsList
+          currentProject={currentProject}
+          action={handleMenuClick}
+        />
       </div>
     </ProjectsStyled>
   );
--- components/ProjectsList/ProjectsList.tsx
+++ components/ProjectsList/ProjectsList.tsx
@@ -2,14 +2,16 @@ import ProjectsListStyled from "./ProjectsListStyled";
 
 interface ProjectsListProps {
   currentProject: number;
+  action: (index: number) => void;
 }
 
-const ProjectsList = ({ currentProject }: ProjectsListProps) => {
+const ProjectsList = ({ currentProject, action }: ProjectsListProps) => {
   return (
     <ProjectsListStyled className="projects-list">
       <h2 className="projects-list__title">PROJECTS</h2>
       <ul className="projects-list__list">
         <li
+          onClick={() => action(1)}
           className={
             currentProject === 1
               ? "projects-list__list-item--current"
